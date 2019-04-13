ddb792bad61323c75c17afffafeeef665bc6fd88
lijunsong <2008jaylijun@sina.com>
2019-04-13 21:46:44 +0800

    consul作为服务注册中心，成功运行

---
+++ common/api-core/src/main/java/com/jasonli/apicore/ApiCoreApplication.java
@@ -0,0 +1,20 @@
+package com.jasonli.apicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SwaggerConfig
+ *
+ * @Description
+ * @Author JasonLi
+ * @Date 2019-04-12 22:42
+ */
+@SpringBootApplication
+public class ApiCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCoreApplication.class, args);
+    }
+
+}
